abf3ccbe7942b79584ff45f7aaa568df114e4015
lw <623656827@qq.com>
2020-07-14 11:53:06 +0800

    finish 需求3

--- src/main/java/com/thoughtworks/basic/Klass.java
+++ src/main/java/com/thoughtworks/basic/Klass.java
@@ -2,6 +2,24 @@ package com.thoughtworks.basic;
 
 public class Klass {
     private int classNumber;
+    private Student[] students;
+    private Teacher teacher;
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public void setStudents(Student[] students) {
+        this.students = students;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
 
     public int getClassNumber() {
         return classNumber;
